e5fd05c3a78fafaea632617ae6aec661d5118ea8
OmegaMiku <95297962+OmegaMiku@users.noreply.github.com>
2021-12-02 13:35:45 +0300

    Delete Stairs.java

--- Stairs.java
+++
@@ -1,7 +0,0 @@
-class Stairs {
-    public static void main(String[] args) {
-        for (int i = 0; i < 51; i++) {
-            System.out.println("Go down a stair step #" + i);
-        }
-    }
-}
